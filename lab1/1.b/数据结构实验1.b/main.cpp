ac599428e755783b4f82163c6cb034f3d35ef2c2
AhaLims <915673037@qq.com>
2018-10-19 18:55:10 +0800

    modified lab1.b

--- lab1/1.b/数据结构实验1.b/main.cpp
+++ lab1/1.b/数据结构实验1.b/main.cpp
@@ -21,17 +21,35 @@ class LinkLine
 public:
 	LinkLine()
 	{
-		cin >> length;
+		//cin >> length;
 		int e;
 		cin >> e;
-		Node * tmp = head = new Node(e);
-		for (int i = 1; i < length; i++)
+		Node * tmp = head = new Node(e % 10);
+		e /= 10;
+		while(e != 0)
+		//for (int i = 1; i < length; i++)
 		{
-			cin >> e;
-			tmp->next_ = new Node(e);
+			
+			//cin >> e;
+			tmp->next_ = new Node(e % 10);
+			e /= 10;
 			tmp = tmp->next_;
 		}
-	}
+	};
+	LinkLine(int e)
+	{
+		Node * tmp = head = new Node(e % 10);
+		e /= 10;
+		while (e != 0)
+			//for (int i = 1; i < length; i++)
+		{
+
+			//cin >> e;
+			tmp->next_ = new Node(e % 10);
+			e /= 10;
+			tmp = tmp->next_;
+		}
+	};
 	int getValue()
 	{
 		Node *tmp = head;
@@ -69,7 +87,9 @@ int main()
 		LinkLine a1, a2;
 		a1.show();
 		a2.show();
-		cout << a1.getValue() + a2.getValue() << endl;
+		LinkLine ans(a1.getValue() + a2.getValue());
+		ans.show();
+		//cout << a1.getValue() + a2.getValue() << endl;
 	}
 	return 0;
 }
